3910cbb2d930a70a27d6ad36c424e8417b004e82
bidisha12 <bidishapaul237@gmail.com>
2020-11-26 23:47:51 +0530

    updated
    Signed-off-by: bidisha12 <bidishapaul237@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
+let obj = {
+    phone: '18602100000',
+    email: 'guestcaredominos@jublfood.com'
+    }
 
 function handleServer(req, res) {
+   if(req.url === '/welcome'){
+       res.write('Welcome to Dominos!');
+       res.write(JSON.stringify({status:200}));
+       res.end();
+
+   }else if(req.url === '/contact'){
+       res.write(JSON.stringify(obj));
+       res.write(JSON.stringify({status:200}));
+       res.end();
+
+   }else{
+    res.write(JSON.stringify({status:404}));
+    res.end();
+
+   }
   
 }
 
+const port = 8081
+httpServer.listen(port, () =>
+  console.log(`listening on port: ${port}`)
+)
+
 module.exports = httpServer;
+
