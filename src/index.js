c4f6e9fcdfb9411bd3c35057055c879bfd85194c
bidisha12 <bidishapaul237@gmail.com>
2020-11-27 09:11:17 +0530

    new
    Signed-off-by: bidisha12 <bidishapaul237@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,22 +10,26 @@ let obj = {
 function handleServer(req, res) {
    if(req.url === '/welcome'){
        res.write('Welcome to Dominos!');
-       res.write(JSON.stringify({status:200}));
-       res.end();
+       res.write('200');
+       //res.write(JSON.stringify({status:200}));
+        res.end();
 
    }else if(req.url === '/contact'){
        res.write(JSON.stringify(obj));
-       res.write(JSON.stringify({status:200}));
+      //res.write(JSON.stringify({status:200}));
+      res.write('200');
        res.end();
 
    }else{
-    res.write(JSON.stringify({status:404}));
+    //res.write(JSON.stringify({status:404}));
+   res.write('404');
     res.end();
+    
 
    }
   
 }
-
+//httpServer.listen(8081);
 const port = 8081
 httpServer.listen(port, () =>
   console.log(`listening on port: ${port}`)
