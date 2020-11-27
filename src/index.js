49ab532fdce0ccd35a91b76b5f03c640ef0be7ad
bidisha12 <bidishapaul237@gmail.com>
2020-11-27 19:08:32 +0530

    new
    Signed-off-by: bidisha12 <bidishapaul237@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,36 +2,30 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-let obj = {
-    phone: '18602100000',
-    email: 'guestcaredominos@jublfood.com'
+const object = {
+    phone: "18602100000",
+    email: "guestcaredominos@jublfood.com"
     }
 
 function handleServer(req, res) {
-   if(req.url === '/welcome'){
-       
-       res.writeHead(200, {'Content-Type': 'text/plain'});
-       res.write('Welcome to Dominos!');
+    if(req.url === "/welcome"){        
+       res.writeHead(200, {"Content-Type" : "text/plain"});
+       res.write("Welcome to Dominos!");
        res.end();
-
-   }else if(req.url === '/contact'){
-       
-       res.writeHead(200, {'Content-Type': 'application/json'});
-       res.write(JSON.stringify(obj));
-       res.end();
-
-   }else{
-    res.statusCode=404;
-    res.end();
 
    }
-  
+   else if(req.url === "/contact"){
+       res.writeHead(200, {"Content-Type" : "application/json"});
+       res.write(JSON.stringify(object));
+       res.end();
+
+   }
+   else{   
+        res.statusCode = 404;
+        res.end("404");
+   }
 }
 
-const port = 8081
-httpServer.listen(port, () =>
-  console.log(`listening on port: ${port}`)
-)
+httpServer.listen(8081);
 
 module.exports = httpServer;
-
